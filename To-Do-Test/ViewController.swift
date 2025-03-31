263c0493e103ca3bee71574d3059709b4e9050c6
ProstoMC <l-63@inbox.ru>
2025-03-31 15:13:39 +0400

    Initial Commit

---
+++ To-Do-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do-Test
+//
+//  Created by admin on 31.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
